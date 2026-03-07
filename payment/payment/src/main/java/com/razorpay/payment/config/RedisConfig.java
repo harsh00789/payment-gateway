7974c27db1c6487ed059a56a521f6a0b4e975cfb
Harsh <harshthakar3611@gmail.com>
2026-03-07 14:34:20 +0530

    redis custom config

--- payment/payment/src/main/java/com/razorpay/payment/config/RedisConfig.java
+++ payment/payment/src/main/java/com/razorpay/payment/config/RedisConfig.java
@@ -1,21 +1,39 @@
 package com.razorpay.payment.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConfiguration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisPassword;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
+import java.net.URI;
+
 @Configuration
 public class RedisConfig {
-
+    @Value("${spring.data.redis.url}")
+    private String redisUrl;
     @Bean
-    public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory("localhost", 6379);
-    }
-    @Bean
-    public StringRedisTemplate redisTemplate(RedisConnectionFactory factory){
-        return new StringRedisTemplate(factory);
+    public LettuceConnectionFactory redisConnectionFactory() {
+        URI uri = URI.create(redisUrl);
 
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
+        config.setHostName(uri.getHost());
+        config.setPort(uri.getPort());
+
+        // Extracting password from "user:password" format
+        String userInfo = uri.getUserInfo();
+        if (userInfo != null && userInfo.contains(":")) {
+            config.setPassword(userInfo.split(":")[1]);
+        }
+
+        return new LettuceConnectionFactory(config);
     }
+@Bean
+public StringRedisTemplate stringRedisTemplate(RedisConnectionFactory connectionFactory) {
+    return new StringRedisTemplate(connectionFactory);
+}
 }
